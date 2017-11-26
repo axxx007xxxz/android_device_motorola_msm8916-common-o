moto8916/android_device_motorola_msm8916-common

msm8916-common: Provide ASensor_getHandle for libmdmcutback
Change-Id: I76a648bacf9c587b9022e8e8ed109846ea838ab6

libshims/Android.mk
@@ -24,3 +24,11 @@ LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+# RIL
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := sensor.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE := libshim_ril
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)

axxx007xxxz/android_device_motorola_msm8916-common-o

Signed-off-by: axxx007xxxz <mattiabrsrn7@gmail.com>

libshims/Android.mk
@@ -25,3 +25,11 @@ LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+# RIL
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := sensor.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE := libshims_ril
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)